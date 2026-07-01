70b813b1681eef09d00c6334c37ab7e16f8b5df5
NMcKenzie77 <n.mckenzie@registechllc.com>
2026-07-01 15:13:52 -0400

    Load dark background fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,5 +5,6 @@ import './styles.css';
 import './hero-compact.css';
 import './walkthrough.css';
 import './hero-shrink.css';
+import './background-fix.css';
 const container = document.getElementById('root');
 createRoot(container).render(<React.StrictMode><App /></React.StrictMode>);
