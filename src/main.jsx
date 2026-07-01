db81214c41687e0c5b7ab9baf4c77fca96c3b87c
NMcKenzie77 <n.mckenzie@registechllc.com>
2026-07-01 12:51:44 -0400

    Load compact hero overrides

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
+import './hero-compact.css';
 
 const container = document.getElementById('root');
 createRoot(container).render(<React.StrictMode><App /></React.StrictMode>);
