422edfaca3ed8aaf372444a64d4fb103a8b6981e
NMcKenzie77 <n.mckenzie@registechllc.com>
2026-07-02 13:56:09 -0400

    Import SEO page styles

--- src/main.jsx
+++ src/main.jsx
@@ -8,5 +8,6 @@ import './hero-shrink.css';
 import './background-fix.css';
 import './alignment-fix.css';
 import './request-flow.css';
+import './seo-pages.css';
 const container = document.getElementById('root');
 createRoot(container).render(<React.StrictMode><App /></React.StrictMode>);
